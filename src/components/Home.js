b5d1e6c9ad60089a93e9bc4007da9a4e62896bd0
Renato Yanez <renatoyanez95@gmail.com>
2023-09-07 18:02:23 -0300

    Hasta aqui llego la evaluacion

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,52 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
-import { AnyContext } from "../context/AnyContext";
+import { useState } from "react";
+import axios from "axios";
 
 const Home = () => {
-  const { text, setText } = useContext(AnyContext);
+  const [search, setSearch] = useState("");
+  const [gifs, setGifs] = useState();
+  console.log(search);
+  console.log(gifs);
+
+  const baseUrl = "https://api.giphy.com/v1/gifs/search";
+  const getGifs = async () => {
+    try {
+      const response = await axios.get(baseUrl, {
+        params: {
+          api_key: "pLURtkhVrUXr3KG25Gy5IvzziV5OrZGa",
+          q: search,
+        },
+      });
+      setGifs(response.data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handleSearch = () => {
+    getGifs();
+  };
 
   return (
     <div>
-      <h1>{text}</h1>
-      <button onClick={() => setText("Hello, world!")}>Click</button>
-      <button>
-        <Link to="/home">Go home</Link>
-      </button>
-      <button>
-        <Link to="/">Go back</Link>
-      </button>
+      <h1>Search Giphy</h1>
+      <form>
+        <input value={search} onChange={handleChange} />
+      </form>
+      <button onClick={handleSearch}>Search</button>
+      <br />
+      {gifs &&
+        gifs.data &&
+        gifs.data.map((eachGif) => {
+          return (
+            <div key={eachGif.id}>
+              <img src={eachGif.images.preview_gif.url} alt="gif" />
+            </div>
+          );
+        })}
     </div>
   );
 };
